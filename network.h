2cfa9cd169c991ab020873a46428d21da6086316
Fesen1337 <andrei.fesenk@gmail.com>
2022-12-21 10:40:50 +0300

    process main logic finished

--- network.h
+++ network.h
@@ -57,32 +57,4 @@ void net(char* buffer, int from_port)
 
 }
 
-void itoa(int n, char s[])//хехе
-{
-    int i, sign;
-
-    if ((sign = n) < 0)  
-        n = -n;          
-    i = 0;
-    do {       
-        s[i++] = n % 10 + '0';   
-    } while ((n /= 10) > 0);     
-    if (sign < 0)
-        s[i++] = '-';
-    s[i] = '\0';
-    reverse(s);
-}
-
-void reverse(char s[])//хаха
-{
-    int i, j;
-    char c;
-
-    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
-        c = s[i];
-        s[i] = s[j];
-        s[j] = c;
-    }
-}
-
 #endif //NETWORK
